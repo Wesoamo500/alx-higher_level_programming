36a26a5d6abdddc26702c973d3e7a60470ffaf28
Suleman Abdul Manan Wesoamo <sulemanabdulmanan@gmail.com>
2023-10-05 13:43:01 +0000

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,3 +6,7 @@ if __name__ == "__main__":
     count = len(sys.argv) - 1
     if count == 0:
         print("0 arguments.")
+    else:
+        print(f"{count} arguments.")
+        for num in range(count):
+            print(f"{num}: {sys.argv[num]}")
