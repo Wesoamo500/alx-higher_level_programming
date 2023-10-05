efb2102e6c85bb95b473fecaf71912c73fbef2aa
Suleman Abdul Manan Wesoamo <sulemanabdulmanan@gmail.com>
2023-10-05 14:24:42 +0000

    Update 100-my_calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
@@ -12,3 +11,7 @@ if __name__ == "__main__":
     if sys.argv[2] not in list(ops.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
