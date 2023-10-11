1955243b0cd7ff1202a56a0093c67d286219b4a7
Suleman Abdul Manan Wesoamo <sulemanabdulmanan@gmail.com>
2023-10-11 12:38:39 +0000

    1-search_replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
+    new_list = []
     for pos in range(len(my_list)):
         if my_list[pos] == search:
-            my_list[pos] = replace
-    return my_list
+            new_list[pos] = replace
+        else:
+            new_list[pos] = my_list[pos]
+    return new_list
